9cff4ef5b7f0b299a2020f4bbdb15503234c68a3
Vicente E. R. Marçal <vicente.marcal@gmail.com>
2017-06-20 15:46:24 -0300

    Acertando apresentação da data!

--- app/models/model.py
+++ app/models/model.py
@@ -7,18 +7,22 @@ def cotar():
     usd_brl, eur_brl, gbp_brl, jpy_brl = Currency('USDBRL'), Currency('EURBRL'), Currency('GBPBRL'), Currency('JPYBRL')
 
     data = datetime.strptime(usd_brl.get_trade_datetime()[:-9], '%Y-%m-%d %H:%M:%S')
-    data = datetime.strftime(data, '%d/%m/%Y as %Hh %Mmin.')
+    data = datetime.strftime(data, '%d/%m/%Y')
 
 
     moeda = {
         'base': 'BRL',
         'date': data,
         'rates': {
-            'GBP': f'{float(gbp_brl.get_ask()):.2f}',
-            'JPY': f'{float(jpy_brl.get_ask()):.2f}',
-            'USD': f'{float(usd_brl.get_ask()):.2f}',
-            'EUR': f'{float(eur_brl.get_ask()):.2f}'
+            'GBP': f'{float(gbp_brl.get_ask()):.3f}',
+            'JPY': f'{float(jpy_brl.get_ask()):.3f}',
+            'USD': f'{float(usd_brl.get_ask()):.3f}',
+            'EUR': f'{float(eur_brl.get_ask()):.3f}'
         }
     }
 
     return moeda
+
+if __name__ == '__main__':
+    dicionario = cotar()
+    print(dicionario)
