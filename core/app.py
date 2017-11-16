ac46c74595b004471e65054aa45734406ca011ab
Vicente Marçal <vicente.marcal@gmail.com>
2017-11-15 23:15:11 -0200

    Fixed path to app.py

--- core/app.py
+++ core/app.py
@@ -1,9 +1,10 @@
+import os
 from flask import Flask, render_template
 from decouple import config
 from core.model import model
 
 
-def create_app(**kwargs):
+def create_app():
 
     app = Flask('core')
     app.config["SECRET_KEY"] = config('SECRET_KEY')
@@ -20,5 +21,6 @@ def create_app(**kwargs):
 
 
 if __name__ == '__main__':
-    app = create_app(debug=True, use_reloader=True)
-    app.run(port=5000)
+    app = create_app()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
