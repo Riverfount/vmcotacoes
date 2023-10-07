78b4342329a2d4fe54e120aca9e7fccc1738764b
Vicente Marçal <vicente.marcal@gmail.com>
2023-10-07 08:02:12 -0300

    feat(vmcotacoes-185) Improve route to retrieve rates information and convert the project into a REST API. (#186)

--- rates/app.py
+++ rates/app.py
@@ -1,8 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+
+from rates.busines_rules.cotacoes import cotar
 
 app = FastAPI()
 
 
 @app.get('/health')
-def health():
+async def health():
     return {'message': 'The Api is 100% healthy.'}
+
+
+@app.get('/rates')
+async def rates():
+    cotacoes = await cotar()
+    if not cotacoes['sucesso']:
+        raise HTTPException(
+            status_code=cotacoes['code'],
+            detail=f'{cotacoes["info"]}. Error Type: {cotacoes["type"]}'
+        )
+    return cotacoes
