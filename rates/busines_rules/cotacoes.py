5b8f57ef3ba527544743ebb6ad22423389fe7056
Vicente Marçal <vicente.marcal@gmail.com>
2023-10-07 08:00:57 -0300

    feat(vmcotacoes-185) Improve route to retrieve rates information and convert the project into a REST API.

--- rates/busines_rules/cotacoes.py
+++ rates/busines_rules/cotacoes.py
@@ -1,43 +1,51 @@
-import requests
+from fastapi import HTTPException
+import httpx
 from datetime import datetime
 from rates.config import settings
+from pydantic import BaseModel
 
 API_KEY = settings.security.API_KEY  # Chave de Autenticação do Serviço
 
 
-def cotar():
+# Models
+
+class RateResponseOk(BaseModel):
+    date: datetime
+
+
+async def cotar():
     # Configurações para realizar a consulta das cotações
 
     currencies = 'BRL, EUR, GBP, BTC'  # Moedas que serão cotadas
 
     # Faz a cotação na API do CurrencyLayer
 
-    query = requests.get(f'http://apilayer.net/api/live?access_key={API_KEY}&currencies={currencies}&format=1')
-    moedas = query.json()  # Converte os dados recebidos em JSON num dicionário Python
+    async with httpx.AsyncClient() as client:
+        query = await client.get(
+            f'http://apilayer.net/api/live?access_key={API_KEY}&currencies={currencies}&source=USD&format=1',
+            timeout=10
+        )
+
+    rates = query.json()  # Converte os dados recebidos em JSON num dicionário Python
 
     # Se houve algum erro na consulta, retorna um dcionário com o Código e Mensagem de Erro
-    if not moedas['success']:
-        mensagem = {
-            'sucesso': moedas['success'],
-            'code': f'{moedas["error"]["code"]}',
-            'info': f'{moedas["error"]["info"]}',
+    if not rates['success']:
+        moedas = {
+            'sucesso': rates['success'],
+            'code': rates['error']['code'],
+            'info': rates['error']['info'],
+            'type': rates['error']['type']
         }
-        return mensagem
-
-    time = datetime.fromtimestamp(moedas['timestamp'])  # Converte o Timestamp em dada em formato para humanos
-
-    # Constrói o diconário de retorno com a data, hora e cotações do Dólar, Euro, Libra Esterlina e Biticoins
-
-    moeda = {
-        'sucesso': moedas['success'],
-        'code': 200,
-        'date': f'{time.day}/{time.month}/{time.year} as {time.hour}h{time.minute}min.',
-        'rates': {
-            'USD': f'{float(moedas["quotes"]["USDBRL"]):.2f}',
-            'EUR': f'{(1 / float(moedas["quotes"]["USDEUR"])) * float(moedas["quotes"]["USDBRL"]):.2f}',
-            'GBP': f'{(1 / float(moedas["quotes"]["USDGBP"])) * float(moedas["quotes"]["USDBRL"]):.2f}',
-            'BTC': f'{(1 / float(moedas["quotes"]["USDBTC"])) * float(moedas["quotes"]["USDBRL"]):.2f}',
+    else:
+        moedas = {
+            'sucesso': rates['success'],
+            'timestamp': rates['timestamp'],
+            'rates': {
+                'USD': f'{float(rates["quotes"]["USDBRL"]):.2f}',
+                'EUR': f'{(1 / float(rates["quotes"]["USDEUR"])) * float(rates["quotes"]["USDBRL"]):.2f}',
+                'GBP': f'{(1 / float(rates["quotes"]["USDGBP"])) * float(rates["quotes"]["USDBRL"]):.2f}',
+                'BTC': f'{(1 / float(rates["quotes"]["USDBTC"])) * float(rates["quotes"]["USDBRL"]):.2f}',
+            }
         }
-    }
 
-    return moeda
+    return moedas
--- rates/config.py
+++ rates/config.py
@@ -1,11 +1,17 @@
+import os
+
 from dynaconf import Dynaconf, Validator
 
+HERE = os.path.dirname(os.path.abspath(__file__))
+
 settings = Dynaconf(
-    envvar_prefix='VM_COTACOES',
-    settings_files=[
-        'core/configs/settings.toml', 'core/configs/.secrets.toml'
-    ],
-    environments=True,
+    envvar_prefix='vm_cotacoes',
+    preload=[os.path.join(HERE, 'default.toml')],
+    settings_files=['settings.toml', '.secrets.toml'],
+    environments=['development', 'production', 'testing'],
+    env_switcher='vm_cotacoes_env',
+    merge_enabled=True,
+    load_dotenv=False
 )
 
 settings.validators.register(
